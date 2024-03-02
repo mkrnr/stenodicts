28c4714ed60cdfe863ca80a00a321b0f8572df62
Martin Koerner <info@mkoerner.de>
2024-03-02 11:39:03 +0100

    Update files

--- common/repeat_stroke.py
+++ common/repeat_stroke.py
@@ -1,9 +1,8 @@
 from os import listdir
 from pathlib import Path
 import json
-
+#from plover import log
 LONGEST_KEY = 3
-
 NUMBER_STROKES={
     "PWH-F":1,
     "PWH-P":2,
@@ -29,24 +28,21 @@ def get_repeated_stroke(number,stroke):
     strokes=''
     for i in range(number):
         strokes+=stroke
+    #log.info('\t%s',strokes)
     return strokes
 
 def lookup(key):
+    #log.info('%s',key)
+
     assert len(key) <= LONGEST_KEY, '%d/%d' % (len(key), LONGEST_KEY)
     
     if len(key) == 2:
         if key[0] in NUMBER_STROKES and key[1] in STROKES:
-            number=NUMBER_STROKES.get(key[0])
             return get_repeated_stroke(NUMBER_STROKES.get(key[0]),STROKES.get(key[1]))
     elif len(key) == 3:
         if key[0] in NUMBER_STROKES and key[1] in NUMBER_STROKES and key[2] in STROKES:
             return get_repeated_stroke(int(str(NUMBER_STROKES.get(key[0]))+str(NUMBER_STROKES.get(key[1]))),STROKES.get(key[2]))
-        elif key[0] in NUMBER_STROKES and key[1] in STROKES:
-            return get_repeated_stroke(NUMBER_STROKES.get(key[0]),STROKES.get(key[1]))
-        elif key[1] in NUMBER_STROKES and key[2] in STROKES:
+        elif key[0] not in NUMBER_STROKES and key[1] in NUMBER_STROKES and key[2] in STROKES:
             return get_repeated_stroke(NUMBER_STROKES.get(key[1]),STROKES.get(key[2]))
-    for entry in key:
-        if entry in NUMBER_STROKES:
-            return " "
-        else:
-            raise KeyError
+    #log.info("\tKeyError")  
+    raise KeyError
